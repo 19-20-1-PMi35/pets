e309731e2337ddc14843e5245d0f623cf3f971ff
Peremoha <dvtytrhenbqrjvg@gmail.com>
2019-12-08 13:34:10 +0200

    some migration

---
+++ pets/pets/Migrations/201912081128174_something.cs
@@ -0,0 +1,18 @@
+﻿namespace pets.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class something : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.tblAnimal", "image");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.tblAnimal", "image", c => c.Binary());
+        }
+    }
+}
